d20cd11ce5250f38714a72a918806d36fb3e11ae
hitori09 <gilbertsaludaga89@gmail.com>
2023-11-10 12:14:09 +0800

    test7

--- veterinary.sql
+++ veterinary.sql
@@ -78,4 +78,20 @@ VALUES (1,"Juan", "De la Cruz", "123 Main St. Manila", 123-456-7890, "juan@examp
     (10, 'Sofia', 'Villianueva', '777 Walnut St, Iloilo', '111-999-3333', 'sofia@example.com');
 
 
+INSERT INTO animals (animalid, name, species, breed, dateofbirth, gender,  color, ownerid)
+VALUES (1, "Fido", "Dog", "Golden Retriver", 2018-03-15, "Male", "Golden", 1),
+       (2, "Whiskers", "Cat", "Siamese", 2019-07-20, "Female", "Cream", 2),
+       (3, "Rocky", "Dog", "German Shepherd", 2017-05-10, "Male", "Black and Tan", 3),
+       (4, "Fluffy", "Cat", "Persian", 2020-01-30, "Male", "White", 4),
+       (5, "Luna", "Dog", "Labrador Retriever", 2019-11-05, "Female", "Yellow", 5),
+       (6, "Mochi", "Cat", "Maine Coon", 2018-09-12, "Male", "Brown Tabby", 6),
+       (7, "Bella", "Dog", "Poodle", 2020-04-05, "Female", "White", 7),
+       (8, "Simba", "Cat", "Bengal", 2018-06-18, "Male", "Spotted Rosetted", 8),
+       (9, 'Max', 'Dog', 'Dachshund', '2020-07-14', 'Male', 'Black and Tan', 9),
+       (10,'Cleo', 'Cat', 'Ragdoll', '2019-12-22', 'Female', 'Seal Point', 10);
+       
+
+
+
+
 
